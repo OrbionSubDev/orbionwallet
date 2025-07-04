d39e32a047804e22e6b6d98d972860903b170867
OrbionSubDev <orbionnetwork@gmail.com>
2025-07-04 19:56:43 +0700

    Delete src directory

--- src/screens/Activity/ActivityScreen.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-const ActivityScreen = () => (
-  <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
-    <Text>Activity</Text>
-  </View>
-);
-export default ActivityScreen;
--- src/screens/Dapps/DappsScreen.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-const DappsScreen = () => (
-  <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
-    <Text>Dapps</Text>
-  </View>
-);
-export default DappsScreen;
